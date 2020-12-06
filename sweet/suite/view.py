0fe0e3eecbd35b1b98ece715db52edd91a157301
David Lai <davidlatwe@gmail.com>
2020-12-07 07:33:10 +0800

    ticking todo

--- sweet/suite/view.py
+++ sweet/suite/view.py
@@ -7,7 +7,6 @@ from .model import SuiteDraftModel
 class SuiteView(QtWidgets.QWidget):
 
     # TODO:
-    # draft (template) suite list (right click to import)
     # existing suite list (right click to load/import, search able)
     # batch context re-resolve, match by resolved package (another window ?)
 
