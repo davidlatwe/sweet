a78ab22108b578cf40b8aa2f5c0e5135c379fb93
David Lai <davidlatwe@gmail.com>
2020-12-06 23:03:08 +0800

    add todo

---
+++ sweet/suite/view.py
@@ -0,0 +1,8 @@
+
+# suite dir
+# suite name
+# save suite button
+# save draft button
+# draft (template) suite list (right click to import)
+# existing suite list (right click to load/import, search able)
+# batch context re-resolve, match by resolved package (another window ?)
