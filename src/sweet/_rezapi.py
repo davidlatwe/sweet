ae34486786fe4c2227e8e7dad5e0f5558b95caad
David Lai <davidlatwe@gmail.com>
2021-12-08 17:31:45 +0800

    refactor SweetSuite

--- src/sweet/_rezapi.py
+++ src/sweet/_rezapi.py
@@ -14,21 +14,29 @@ Suite = suite.Suite
 SuiteError = suite.SuiteError
 
 
-class SweetSuite(Suite):
+class _Suite(Suite):
+    @classmethod
+    def from_dict(cls, d):
+        s = cls.__new__(cls)
+        s.load_path = None
+        s.tools = None
+        s.tool_conflicts = None
+        s.contexts = d["contexts"]
+        if s.contexts:
+            s.next_priority = max(x["priority"]
+                                  for x in s.contexts.values()) + 1
+        else:
+            s.next_priority = 1
+        return s
 
-    def __init__(self, live=True):
+
+class SweetSuite(_Suite):
+
+    def __init__(self):
         super(SweetSuite, self).__init__()
         self.description = ""
-        self._is_live = live
+        self._is_live = True
         self._requests = None
-        # TODO: Implementing `live` checkBox, and it should reflect loaded
-        #   suite's "live_resolve" value.
-
-    def add_description(self, text):
-        self.description = text
-
-    def is_live(self):
-        return self._is_live
 
     def context(self, name):
         """Get a context.
@@ -56,85 +64,6 @@ class SweetSuite(Suite):
         data["context"] = context
         data["loaded"] = True
         return context
-
-    def to_dict(self):
-        data = super(SweetSuite, self).to_dict()
-        data["description"] = self.description
-        data["live_resolve"] = self._is_live
-        data["requests"] = {
-            cname: [str(r) for r in self.context(cname).requested_packages()]
-            for cname in self.context_names
-        }
-        return data
-
-    @classmethod
-    def from_dict(cls, d):
-        suite.Suite = SweetSuite
-        s = super(SweetSuite, cls).from_dict(d)
-        s.description = d.get("description", "")
-        s._is_live = d.get("live_resolve", False)
-        s._requests = d.get("requests")
-        return s
-
-    def sorted_context_names(self):
-        ctxs = self.contexts
-        return sorted(ctxs.keys(), key=lambda x: ctxs[x]["priority"])
-
-    def read_context(self, name, entry, default=None):
-        data = self._context(name)
-        return data.get(entry, default)
-
-    def has_context(self, name):
-        """Is context name exists in suite ?"""
-        return name in self.contexts
-
-    def rename_context(self, old_name, new_name):
-        """Rename context
-
-        Tools will be flushed after rename.
-
-        Args:
-            old_name (str): Original context name.
-            new_name (str): New context name.
-
-        """
-        if old_name not in self.contexts:
-            raise SuiteError("Context not in suite: %r" % old_name)
-
-        data = self.contexts.pop(old_name)
-        data["name"] = new_name
-        self.contexts[new_name] = data
-        self._flush_tools()
-
-    def update_context(self, name, context):
-        """Add or update a context to the suite
-
-        If `name` exist in suite, `context` will be replaced and tools
-        be reset. This is to avoid removing and adding back context under
-        same name when re-requesting packages, which will lead to context
-        priority change.
-
-        Args:
-            name (str): Name to store the context under.
-            context (ResolvedContext): Context to add/update.
-
-        """
-        # so we don't need to worry about priority change if remove and re-add
-        if not context.success:
-            raise SuiteError("Context is not resolved: %r" % name)
-
-        if name in self.contexts:
-            # update with priority preserved
-            data = self.contexts[name]
-            data["context"] = context.copy()
-            data["tool_aliases"] = {}
-            data["hidden_tools"] = set()
-            self._flush_tools()
-        else:
-            self.add_context(name, context)
-
-    # Exposing protected member that I'd like to use.
-    update_tools = Suite._update_tools
 
     def save(self, path, verbose=False):
         """Save the suite to disk.
@@ -220,6 +149,90 @@ class SweetSuite(Suite):
                 }
 
             create_forwarding_script(filepath, **kwargs)
+
+    def to_dict(self):
+        data = super(SweetSuite, self).to_dict()
+        data["description"] = self.description
+        data["live_resolve"] = self._is_live
+        data["requests"] = {
+            cname: [str(r) for r in self.context(cname).requested_packages()]
+            for cname in self.context_names
+        }
+        return data
+
+    @classmethod
+    def from_dict(cls, d):
+        s = super(SweetSuite, cls).from_dict(d)
+        s.description = d.get("description", "")
+        s._is_live = d.get("live_resolve", False)
+        s._requests = d.get("requests")
+        return s
+
+    def is_live(self):
+        return self._is_live
+
+    def add_description(self, text):
+        self.description = text
+
+    def sorted_context_names(self):
+        ctxs = self.contexts
+        return sorted(ctxs.keys(), key=lambda x: ctxs[x]["priority"])
+
+    def read_context(self, name, entry, default=None):
+        data = self._context(name)
+        return data.get(entry, default)
+
+    def has_context(self, name):
+        """Is context name exists in suite ?"""
+        return name in self.contexts
+
+    def rename_context(self, old_name, new_name):
+        """Rename context
+
+        Tools will be flushed after rename.
+
+        Args:
+            old_name (str): Original context name.
+            new_name (str): New context name.
+
+        """
+        if old_name not in self.contexts:
+            raise SuiteError("Context not in suite: %r" % old_name)
+
+        data = self.contexts.pop(old_name)
+        data["name"] = new_name
+        self.contexts[new_name] = data
+        self._flush_tools()
+
+    def update_context(self, name, context):
+        """Add or update a context to the suite
+
+        If `name` exist in suite, `context` will be replaced and tools
+        be reset. This is to avoid removing and adding back context under
+        same name when re-requesting packages, which will lead to context
+        priority change.
+
+        Args:
+            name (str): Name to store the context under.
+            context (ResolvedContext): Context to add/update.
+
+        """
+        # so we don't need to worry about priority change if remove and re-add
+        if not context.success:
+            raise SuiteError("Context is not resolved: %r" % name)
+
+        if name in self.contexts:
+            # update with priority preserved
+            data = self.contexts[name]
+            data["context"] = context.copy()
+            data["tool_aliases"] = {}
+            data["hidden_tools"] = set()
+            self._flush_tools()
+        else:
+            self.add_context(name, context)
+
+    # Exposing protected member that I'd like to use.
+    update_tools = Suite._update_tools
 
 
 def read_suite_description(filepath):
