68e527bb8f99f309f32baa3aaa5cce662eaf6ccc
davidlatwe <davidlatwe@gmail.com>
2021-12-11 21:39:59 +0800

    update rezconfig.py

--- src/sweet/rezplugins/command/rezconfig.py
+++ src/sweet/rezplugins/command/rezconfig.py
@@ -36,9 +36,9 @@ sweet = {
     # callback
     "on_suite_saved_callback": on_suite_saved_callback,
 
-}
+    # wip:
+    #   If not empty, internal package version will be omitted
+    #   in package versions' auto completion list.
+    "omit_internal_version": "",
 
-# wip:
-#   If not empty, internal package version will be omitted
-#   in package versions' auto completion list.
-omit_internal_version = ""
+}
