83721de88d91a5fb269257c78f348838884808d4
davidlatwe <davidlatwe@gmail.com>
2022-01-21 03:43:46 +0800

    ditch .qss, construct style-sheet programmatically (wip)

--- src/sweet/gui/resources.py
+++ src/sweet/gui/resources.py
@@ -1,17 +1,15 @@
 
-import os
 from collections import OrderedDict as odict
 from ._vendor.Qt5 import QtGui
+from .style import BaseLightTheme, BaseDarkTheme
 
-dirname = os.path.dirname(__file__)
-_cache = {}
 _themes = odict()
 
 
 def load_themes():
     _themes.clear()
     for theme in default_themes():
-        _themes[theme["name"]] = theme
+        _themes[theme.name] = theme
 
 
 def theme_names():
@@ -28,116 +26,14 @@ def load_theme(name=None):
     else:
         theme = next(iter(_themes.values()))
 
-    source = theme["source"]
-    keywords = theme.get("keywords", dict())
-
-    if any(source.endswith(ext) for ext in [".css", ".qss"]):
-        if not os.path.isfile(source):
-            print("Theme stylesheet file not found: %s" % source)
-            return
-        else:
-            with open(source) as f:
-                css = f.read()
-    else:
-        # plain css code
-        css = source
-
-    _cache["_keywordsCache_"] = keywords
-
-    return format_stylesheet(css)
-
-
-def format_stylesheet(css):
-    try:
-        return css % _cache["_keywordsCache_"]
-    except KeyError as e:
-        print("Stylesheet format failed: %s" % str(e))
-        return ""
+    return theme.style_sheet()
 
 
 def default_themes():
     Resources.load()
     return [
-        {
-            "name": "sweet-dark",
-            "source": os.path.join(dirname, "sweet.qss"),
-            "keywords": {
-                "primary.focus": "#76654B",
-                "primary.bright": "#654F3E",
-                "primary.dim": "#493D35",
-
-                "secondary.focus": "#4B6375",
-                "secondary.bright": "#3E6166",
-                "secondary.dim": "#36494A",
-
-                "surface.bright": "#262626",
-                "surface.dim": "#212121",
-
-                "background.bright": "#1F1F1F",
-                "background.dim": "#1D1D1D",
-
-                "border.bright": "#191919",
-                "border.dim": "#242424",
-
-                "error.bright": "#C62828",
-                "error.dim": "#891B1B",
-
-                "warning.bright": "#DC9029",
-                "warning.dim": "#B8843A",
-
-                "on.bright.primary": "#212121",
-                "on.bright.secondary": "#212121",
-                "on.bright.surface": "#A49884",
-                "on.bright.background": "#6F6350",
-                "on.bright.error": "#FFFFFF",
-
-                "on.dim.primary": "#39322D",
-                "on.dim.secondary": "#4B403E",
-                "on.dim.surface": "#4D4D4D",
-                "on.dim.background": "#3F3F3F",
-                "on.dim.error": "#9E9E9E",
-            }
-        },
-        {
-            "name": "sweet-light",
-            "source": os.path.join(dirname, "sweet.qss"),
-            "keywords": {
-                "primary.focus": "#76654B",
-                "primary.bright": "#654F3E",
-                "primary.dim": "#493D35",
-
-                "secondary.focus": "#4B6375",
-                "secondary.bright": "#3E6166",
-                "secondary.dim": "#36494A",
-
-                "surface.bright": "#C8BFB2",
-                "surface.dim": "#B0A798",
-
-                "background.bright": "#B3AA9E",
-                "background.dim": "#A0988D",
-
-                "border.bright": "#917F6F",
-                "border.dim": "#A08F89",
-
-                "error.bright": "#C62828",
-                "error.dim": "#891B1B",
-
-                "warning.bright": "#DC9029",
-                "warning.dim": "#B8843A",
-
-                "on.bright.primary": "#212121",
-                "on.bright.secondary": "#212121",
-                "on.bright.surface": "#342C25",
-                "on.bright.background": "#2C251F",
-                "on.bright.error": "#FFFFFF",
-
-                "on.dim.primary": "#39322D",
-                "on.dim.secondary": "#4B403E",
-                "on.dim.surface": "#4D4D4D",
-                "on.dim.background": "#3F3F3F",
-                "on.dim.error": "#9E9E9E",
-            }
-        },
+        BaseLightTheme(),
+        BaseDarkTheme(),
     ]
 
 
---
+++ src/sweet/gui/style.py
@@ -0,0 +1,240 @@
+
+from dataclasses import dataclass
+import qstylizer.style
+
+
+@dataclass
+class HSL:
+    """
+    The value of s, v, l and a in hsv(), hsva() hsl() or hsla() must
+    all be in the range 0-255 or with percentages, the value of h must
+    be in the range 0-359.
+    """
+    h: float
+    s: float
+    l: float
+
+    def __str__(self):
+        return f"hsl({self.h}, {self.s}%, {self.l}%)"
+
+    def __mul__(self, other: float):
+        self.l *= other
+        return self
+
+    def __add__(self, other: float):
+        self.l += other
+        return self
+
+
+@dataclass
+class Palette:
+    # primary color platte
+    primary: HSL
+    primary_bright: HSL
+    primary_dimmed: HSL
+    # secondary color platte
+    secondary: HSL
+    secondary_bright: HSL
+    secondary_dimmed: HSL
+    # base colors
+    surface: HSL
+    background: HSL
+    # additional colors
+    error: HSL
+    warning: HSL
+    # texts
+    on_primary: HSL
+    on_secondary: HSL
+    on_surface: HSL
+    on_background: HSL
+    on_error: HSL
+    on_warning: HSL
+
+
+class BaseLightTheme(object):
+    name = "sweet-light"
+    palette = Palette(
+        primary=HSL(35.67, 100.00, 57.45),          # Orange 400
+        primary_bright=HSL(53.88, 100.00, 61.57),   # Yellow 500
+        primary_dimmed=HSL(35.91, 100.00, 75.10),   # Orange 200
+
+        secondary=HSL(15.88, 15.32, 56.47),         # Brown 300
+        secondary_bright=HSL(15.71, 17.50, 47.06),  # Brown 400
+        secondary_dimmed=HSL(16.00, 15.79, 81.37),  # Brown 100
+
+        surface=HSL(0.00, 0.00, 74.12),             # Grey 400
+        background=HSL(0.00, 0.00, 87.84),          # Grey 300
+
+        error=HSL(11.95, 100.00, 43.33),            # Deep Orange A700
+        warning=HSL(40.24, 100.00, 50.00),          # Amber A700
+
+        on_primary=HSL(0.00, 0.00, 25.88),          # Grey 800
+        on_secondary=HSL(0.00, 0.00, 25.88),        # Grey 800
+        on_surface=HSL(200.00, 15.63, 62.35),       # Blue Grey 300
+        on_background=HSL(200.00, 15.63, 62.35),    # Blue Grey 300
+        on_error=HSL(0.00, 0.00, 12.94),            # Grey 900
+        on_warning=HSL(0.00, 0.00, 12.94),          # Grey 900
+    )
+
+    def __init__(self):
+        self._composed = None
+        self.qss = qstylizer.style.StyleSheet()
+
+    def style_sheet(self, refresh=False):
+        if self._composed is None or refresh:
+            self.compose_styles()
+            self._composed = self.qss.toString()
+        return self._composed
+
+    def compose_styles(self):
+        self._global()
+        self._widget()
+        self._label()
+        self._button()
+        self._check_box()
+        self._menu()
+        self._frame()
+
+    def _global(self):
+        self.qss.setValues(
+            outline="none",
+            color=self.palette.on_primary,  # text color
+            borderColor=self.palette.primary_dimmed,
+            fontFamily="Open Sans",
+        )
+
+    def _widget(self):
+        self.qss.QWidget.setValues(
+            backgroundColor=self.palette.background,
+        )
+        self.qss.QWidget["focus"].setValues(
+            border=f"1px solid {self.palette.primary_bright}",
+        )
+        self.qss.QWidget["disabled"].setValues(
+            color=self.palette.primary,
+        )
+
+    def _label(self):
+        self.qss.QLabel.setValues(
+            paddingTop="2px",
+            paddingBottom="2px",
+        )
+
+    def _button(self):
+        self.qss.QPushButton.setValues(
+            backgroundColor=self.palette.secondary,
+            border=f"1px solid {self.palette.secondary_dimmed}",
+            borderRadius="20px",
+            minHeight="30px",
+            padding="5px",
+        )
+        self.qss.QPushButton["hover"].setValues(
+            border=f"1px solid {self.palette.secondary_bright}",
+        )
+        self.qss.QPushButton["pressed"].setValues(
+            backgroundColor=self.palette.secondary_bright,
+            border=f"1px solid {self.palette.secondary_bright}",
+        )
+
+    def _check_box(self):
+        self.qss.QCheckBox.setValues(
+            spacing="5px",
+            marginBottom="2px",
+        )
+        self.qss.QCheckBox["focus"].setValues(
+            border="none",
+        )
+        self.qss.QCheckBox["indicator"].setValues(
+            width="15px",
+            height="15px",
+            backgroundColor=self.palette.background,
+        )
+        self.qss.QCheckBox["indicator"]["unchecked"].setValues(
+            image="url(:/icons/checkbox_unchecked)",
+        )
+        self.qss.QCheckBox["indicator"]["unchecked"]["disabled"].setValues(
+            image="url(:/icons/checkbox_unchecked_dim)",
+        )
+        self.qss.QCheckBox["indicator"]["checked"].setValues(
+            image="url(:/icons/checkbox_checked)",
+        )
+        self.qss.QCheckBox["indicator"]["checked"]["disabled"].setValues(
+            image="url(:/icons/checkbox_checked_dim)",
+        )
+        self.qss.QCheckBox["indicator"]["indeterminate"].setValues(
+            image="url(:/icons/checkbox_indeterminate)",
+        )
+        self.qss.QCheckBox["indicator"]["indeterminate"]["disabled"].setValues(
+            image="url(:/icons/checkbox_indeterminate_dim)",
+        )
+
+    def _menu(self):
+        self.qss.QMenu.setValues(
+            paddingTop="2px",
+            paddingBottom="2px",
+            backgroundColor=self.palette.surface,
+            border=f"1px solid transparent",
+        )
+        self.qss.QMenu["item"].setValues(
+            padding="5px 16px 5px 16px",
+            marginLeft="5px",
+        )
+        self.qss.QMenu["item"]["!selected"].setValues(
+            color=self.palette.on_surface,
+        )
+        self.qss.QMenu["item"]["selected"].setValues(
+            color=self.palette.on_secondary,
+            backgroundColor=self.palette.secondary,
+        )
+        self.qss.QMenu["item"]["disabled"].setValues(
+            color=self.palette.surface,
+        )
+        self.qss.QMenu["indicator"].setValues(
+            width="12px",
+            height="12px",
+            marginLeft="4px",
+        )
+        self.qss.QMenu["indicator"]["non-exclusive"]["checked"].setValues(
+            image="url(:/icons/checkbox_checked)",
+        )
+        self.qss.QMenu["indicator"]["non-exclusive"]["unchecked"].setValues(
+            image="url(:/icons/checkbox_unchecked)",
+        )
+        self.qss.QMenu["indicator"]["exclusive"]["checked"].setValues(
+            image="url(:/icons/checkbox_checked)",
+        )
+        self.qss.QMenu["indicator"]["exclusive"]["unchecked"].setValues(
+            image="url(:/icons/checkbox_unchecked)",
+        )
+
+    def _frame(self):
+        # separator line
+        self.qss[".QFrame"].setValues(
+            color=self.palette.background,
+        )
+
+
+class BaseDarkTheme(BaseLightTheme):
+    name = "sweet-dark"
+    palette = Palette(
+        primary=HSL(35.67, 100.00, 57.45),  # Orange 400
+        primary_bright=HSL(53.88, 100.00, 61.57),  # Yellow 500
+        primary_dimmed=HSL(35.91, 100.00, 75.10),  # Orange 200
+
+        secondary=HSL(15.88, 15.32, 56.47),  # Brown 300
+        secondary_bright=HSL(15.71, 17.50, 47.06),  # Brown 400
+        secondary_dimmed=HSL(16.00, 15.79, 81.37),  # Brown 100
+
+        surface=HSL(0.00, 0.00, 74.12),  # Grey 400
+        background=HSL(0.00, 0.00, 87.84),  # Grey 300
+
+        error=HSL(11.95, 100.00, 43.33),  # Deep Orange A700
+        warning=HSL(40.24, 100.00, 50.00),  # Amber A700
+
+        on_primary=HSL(0.00, 0.00, 25.88),  # Grey 800
+        on_secondary=HSL(0.00, 0.00, 25.88),  # Grey 800
+        on_surface=HSL(200.00, 15.63, 62.35),  # Blue Grey 300
+        on_background=HSL(200.00, 15.63, 62.35),  # Blue Grey 300
+        on_error=HSL(0.00, 0.00, 12.94),  # Grey 900
+        on_warning=HSL(0.00, 0.00, 12.94),  # Grey 900
+    )
